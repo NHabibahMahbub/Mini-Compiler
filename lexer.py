84977959c47f9b2d4081b9e68e4c8bfcd164f60a
NHabibahMahbub <habs101300@gmail.com>
2025-10-18 21:56:57 +0600

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,26 +1,52 @@
 import ply.lex as lex
 class MiniLexer:
     tokens = [
-        'ID', 'NUMBER', 'STRING',            # <-- added STRING
+        'ID', 'NUMBER', 'STRING',          
         'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
         'ASSIGN',
         'LT', 'LE', 'GT', 'GE', 'EQ', 'NE',
-        'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE',
+        'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'LBRACKET', 'RBRACKET',
         'SEMICOLON', 'COMMA'
     ]
 
     reserved = {
-        'int': 'INT',
-        'float': 'FLOAT',
-        'if': 'IF',
-        'else': 'ELSE',
-        'while': 'WHILE',
-        'print': 'PRINT'
-    }
-
+    'int': 'INT',
+    'float': 'FLOAT',
+    'if': 'IF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'print': 'PRINT',
+    'auto': 'AUTO',
+    'break': 'BREAK',
+    'case': 'CASE',
+    'char': 'CHAR',
+    'const': 'CONST',
+    'continue': 'CONTINUE',
+    'default': 'DEFAULT',
+    'do': 'DO',
+    'double': 'DOUBLE',
+    'enum': 'ENUM',
+    'extern': 'EXTERN',
+    'for': 'FOR',
+    'goto': 'GOTO',
+    'long': 'LONG',
+    'register': 'REGISTER',
+    'return': 'RETURN',
+    'short': 'SHORT',
+    'signed': 'SIGNED',
+    'sizeof': 'SIZEOF',
+    'static': 'STATIC',
+    'struct': 'STRUCT',
+    'switch': 'SWITCH',
+    'typedef': 'TYPEDEF',
+    'union': 'UNION',
+    'unsigned': 'UNSIGNED',
+    'void': 'VOID',
+    'volatile': 'VOLATILE',
+}
     tokens += list(reserved.values())
 
-    # Operators
+
     t_PLUS = r'\+'
     t_MINUS = r'-'
     t_TIMES = r'\*'
@@ -33,12 +59,12 @@ class MiniLexer:
     t_GT = r'>'
     t_EQ = r'=='
     t_NE = r'!='
-
-    # Punctuation
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     t_LBRACE = r'\{'
     t_RBRACE = r'\}'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
     t_SEMICOLON = r';'
     t_COMMA = r','
 
@@ -63,8 +89,8 @@ class MiniLexer:
 
     # Strings
     def t_STRING(self, t):
-        r'\"([^\\\n]|(\\.))*?\"'   # matches "anything inside quotes"
-        t.value = t.value[1:-1]    # remove the surrounding quotes
+        r'\"([^\\\n]|(\\.))*?\"'   
+        t.value = t.value[1:-1]    
         return t
 
     # Identifiers
